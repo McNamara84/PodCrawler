8136f38174dc444532ab9bc8d17fce5a0727f5f9
McNamara <holgerehrmann@gmail.com>
2023-02-06 11:30:03 +0100

    versch. Bereiche kommentiert zur besseren Erklärung und Übersichtlichkeit

--- start.py
+++ start.py
@@ -1,18 +1,21 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # Verifikation für die Spotify API
 client_id = "cb68bb0f66804f22be4e0ca5c6ca66b5"
 client_secret = "d2f2bd91a62747338b6ef320fc719ce9"
-# Variable
+
+# Variablen, die der Nutzer festlegen soll
 suchbegriff = "Museum"
 limit = 10
 
+# Spotify API Objekt erstellen
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #Spotify API Objekt erstellen
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+# Funktionsaufruf für Suche
 result = sp.search(suchbegriff)
 result['tracks']['items'][0]['artists']
 
-#Test
+# Testbereich
 print(result)
