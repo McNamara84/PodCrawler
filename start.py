27dfbcb17ab928ea8817b806d34e95f9ee23ac5a
McNamara <holgerehrmann@gmail.com>
2023-02-10 16:33:03 +0100

    write_to_file() ergänzt
    Leider bisher nur für Strings funktionierend und nicht für Dictionaries.

--- start.py
+++ start.py
@@ -8,6 +8,7 @@ client_secret = "d2f2bd91a62747338b6ef320fc719ce9"
 # Variablen, die der Nutzer festlegen soll
 suchbegriff = "Museum"
 limit = 10
+dateiname = "Ergebnisse.json"
 
 # Spotify API Objekt erstellen
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
@@ -19,9 +20,14 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 # Funktionsaufruf für Suche
 result = sp.search(suchbegriff)
 
-# TODO Holger: JSON-String formatieren und als Datei abspeichern.
+# BUG Holger: Fehlerbehebeung -> Funktion speichert bisher nur Strings und keine Dictionaries.
+def write_to_file(filename, text):
+    with open(filename, 'w') as file:
+        file.write(text)
+    print("Datei wurde erfolgreich gespeichert.")
 
 result['tracks']['items'][0]['artists']
 
 # Testbereich
 print(result)
+write_to_file(dateiname, result)
