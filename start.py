bd032e1317718c9fbc6cb8fcbfaed9956b33b696
Kito77 <katja.geo@gmail.com>
2023-02-16 10:20:55 +0100

    Update start.py
    hier!

--- start.py
+++ start.py
@@ -7,6 +7,8 @@ client_secret = "d2f2bd91a62747338b6ef320fc719ce9"
 
 #Suchbegriff für den Webcrawler
 sterm = ""
+resultcount = 20
+resultcountmax = 100
 
 def get_search_string():
     sterm = input("Gebe den Suchbegriff ein:\n")
@@ -16,4 +18,20 @@ def get_search_string():
     if (len(sterm)>=50):
         print("Das ist zu viel! Maximal 50 Zeichen bitte.")
         get_search_string()
-get_search_string()
+
+
+def get_result_count():
+    resultcount= input("Wieviel Ergebnisse möchtest du erhalten?")
+    try: int(resultcount)
+    except ValueError:
+        if (resultcount==""):
+            resultcount=resultcountmax
+        else: 
+            print("DU dumme Nuss! EINE ZAAAHL!")
+            get_result_count()
+    if (resultcount>=100):
+        print("Das ist doch zu viel! Max. {}.".format(resultcountmax))
+        get_result_count()
+
+#get_search_string()
+get_result_count()
